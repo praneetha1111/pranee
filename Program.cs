c3aa507cccc62b59c972eea8200b71fbefb33eb4
praneetha1111 <94691061+praneetha1111@users.noreply.github.com>
2021-11-19 18:33:27 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+string secretWord = "giraffe";
+string guess = " ";
+do
+{
+    Console.Write("Enter Guess: ");
+    guess = Console.ReadLine();
+ 
+}
+while (guess != secretWord);
+Console.WriteLine("YOU WIN!");
+
+   
+Console.ReadLine();
