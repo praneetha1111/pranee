1938b7426fe8861e6ae15c4ca19b7ab0b09f3db2
praneetha1111 <94691061+praneetha1111@users.noreply.github.com>
2021-11-22 14:48:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('Object Implementation');
+let person = {
+    name:'Damon',
+    age:43
+};
+ console.log(person.name);
+ person.name = 'Stefan';
+ console.log("now the name of the person is:");
+ console.log(person.name);
+ 
+
