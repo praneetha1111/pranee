58f8cc36529454eb5704ed36d76860606573472b
praneetha1111 <94691061+praneetha1111@users.noreply.github.com>
2021-11-22 16:22:14 +0530

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Giraffe
+{
+    class Maximum
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(getMax(59, 6));
+            Console.ReadLine();        }
+        static int getMax(int num1, int num2)
+        {
+            int result;
+            if (num1 > num2)
+            {
+                result = num1;
+            }
+            else
+            {
+                result = num2;
+            }
+            return result;
+        }
+    }
+}
